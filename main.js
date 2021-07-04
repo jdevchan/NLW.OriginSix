3be4c474c96dc04285b54d3a85e6ee8b62c8bd0d
jdevchan <jdevchan@gmail.com>
2021-07-04 00:43:15 -0300

    Aula 3
    Aula 3

--- main.js
+++ main.js
@@ -29,3 +29,29 @@ window.addEventListener('scroll', function() {
     header.classList.remove('scroll')
   }
 })
+
+// TESTIMONIAL SLIDER SWIPER
+const swiper = new Swiper('.swiper-container', {
+  slidesPerview: 1,
+  pagination: {
+    el: '.swiper-pagination'
+  },
+  mousewheel: true,
+  keyboard: true
+})
+
+// SCROLLREVEAL SUAVISAR EXIBIÇÃO DE ITENS
+const scrollReveal = ScrollReveal({
+  origin: 'top',
+  distance: '30px',
+  duration: 700,
+  reset: true
+})
+
+scrollReveal.reveal(
+  `#home .image, #home .text,
+   #about .image, #about.text,
+   #services .header, #services .card,
+   #testimonials .header, #testimonials .testimonials,
+   #contact .text, #contact .links
+  `, {interval: 100})
